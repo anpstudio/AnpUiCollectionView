59fcd4ab28c6c4d45b81b07cd4b966bd7f257d29
Antonio Carabantes Millan <antocara@gmail.com>
2015-07-26 13:30:14 +0200

    * Initial Commit * Created project structure

---
+++ AnpUiCollectionView/AnpUiCollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AnpUiCollectionView
+//
+//  Created by Antonio Carabantes Millan on 26/7/15.
+//  Copyright (c) 2015 anpstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AnpUiCollectionView/AnpUiCollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AnpUiCollectionView
+//
+//  Created by Antonio Carabantes Millan on 26/7/15.
+//  Copyright (c) 2015 anpstudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
