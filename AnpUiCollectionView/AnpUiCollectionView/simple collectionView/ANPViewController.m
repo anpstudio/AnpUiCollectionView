a1212bbff83618ee332d05243e4a9be2d6a79b33
Antonio Carabantes Millan <antocara@gmail.com>
2015-07-26 21:11:07 +0200

    *First Example UICollectionViewController

---
+++ AnpUiCollectionView/AnpUiCollectionView/simple collectionView/ANPViewController.m
@@ -0,0 +1,118 @@
+//
+//  ANPViewController.m
+//  AnpUiCollectionView
+//
+//  Created by Antonio Carabantes Millan on 26/7/15.
+//  Copyright (c) 2015 anpstudio. All rights reserved.
+//
+
+#import "ANPViewController.h"
+
+@interface ANPViewController (){
+    NSArray *arrayColors;
+}
+
+@end
+
+@implementation ANPViewController
+
+static NSString * const reuseIdentifier = @"Cell";
+
+
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Register cell classes
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    
+    // Do any additional setup after loading the view.
+    //DUMMY DATA
+    const NSInteger numberOfColor = 100;
+    
+    NSMutableArray *tempArray = [NSMutableArray arrayWithCapacity:100];
+    for (NSInteger i = 0; i < numberOfColor; i++) {
+        CGFloat redValue = (arc4random() % 255) /255.0f;
+        CGFloat blueValue = (arc4random() % 255) /255.0f;
+        CGFloat greenValue = (arc4random() % 255) /255.0f;
+        
+        
+        [tempArray addObject:[UIColor colorWithRed:redValue green:greenValue blue:blueValue alpha:1.0f]];
+    }
+    
+    arrayColors = [NSArray arrayWithArray:tempArray];
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
+    return 1;
+}
+
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return [arrayColors count];
+}
+
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell
+    
+    cell.backgroundColor = [arrayColors objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+#pragma mark <UICollectionViewDelegate>
+
+/*
+// Uncomment this method to specify if the specified item should be highlighted during tracking
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+}
+*/
+
+/*
+// Uncomment this method to specify if the specified item should be selected
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+*/
+
+/*
+// Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+- (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+}
+
+- (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+}
+*/
+
+@end
